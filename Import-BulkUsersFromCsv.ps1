22df0b3597d1d272028d0e04a7477d054eacdfb8
gkm-automation <52336739+gkm-automation@users.noreply.github.com>
2019-07-28 17:44:40 +0530

    Create AD users from CSV
    Create AD users from CSV

---
+++ Import-BulkUsersFromCsv.ps1
@@ -0,0 +1,29 @@
+﻿$File = Import-Csv -Path ".\WSC2019_TP39_Module_B_Users.csv"
+## Space should be there for property "Organizational Unit"
+$File |Select-Object| Sort-Object -Property "Organizational Unit" -Unique|Select-Object 'Organizational Unit'|ForEach-Object {
+    New-ADOrganizationalUnit $_.'Organizational Unit'
+}
+## You have to Define Group Scope Universal | Domail Local | Global
+$File|Select-Object|Sort-Object 'Group' -Unique|Select-Object 'Group'|ForEach-Object {
+    New-ADGroup $_.Group -GroupScope Global
+}
+
+$File|ForEach-Object {
+    New-ADUser `
+    -Name ($_.'Given Name'+' '+$_.Surname) `
+    -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force) `
+    -City $_.City `
+    -Company $_.Company `
+    -OtherAttributes @{co=$_.Country}`
+    -DisplayName ($_.'Given Name'+' '+$_.Surname) `
+    -EmailAddress $_.'E-Mail' `
+    -Enabled $true `
+    -GivenName $_.'Given Name' `
+    -HomeDirectory '\\dc2\Homes\%username%' `
+    -HomeDrive 'H:\' `
+    -PasswordNeverExpires $true `
+    -Path ("ou="+$_."organizational unit"+",dc=wsc2019,dc=ru")  `
+    -SamAccountName $_.ID `
+    -Surname $_.Surname
+}
+
