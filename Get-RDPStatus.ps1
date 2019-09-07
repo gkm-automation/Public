a557521eee6c3ebc114f789e4f8b10de2bff2e93
gkm-automation <52336739+gkm-automation@users.noreply.github.com>
2019-09-07 21:01:51 +0530

    Get-RDP

---
+++ Get-RDPStatus.ps1
@@ -0,0 +1,32 @@
+﻿[cmdletBinding()]
+param(
+[string]$RemoteComputer = $env:COMPUTERNAME
+)
+
+$ParamHash = [ordered]@{
+              Ping = $false
+              FQDNCheck = $false
+              RDPPortCheck = $false
+              RDPServicecheck = $false
+              }
+
+Write-Host "Checking RDP Status for the Server $RemoteComputer.........." -ForegroundColor Yellow
+
+
+try {
+
+            if((Test-Connection -ComputerName $RemoteComputer -Count 1 -Quiet -ErrorAction SilentlyContinue -ErrorVariable test) -and ([System.Net.Dns]::GetHostEntry($RemoteComputer))) {
+
+            $ParamHash.Ping = $true
+            $ParamHash.FQDNCheck = $true
+            if(New-Object Net.sockets.TcpClient($RemoteComputer,3389)) { $ParamHash.RDPPortCheck = $true }
+           
+            }
+
+}
+catch{
+Write-Host " $_Exception.Message"
+}
+
+$ParamHash
+
